030f6fdc4753d18c2b0d3513bb96dd171dee6a48
David Marsh <40401344+Daveym1@users.noreply.github.com>
2023-02-05 18:23:42 +0000

    added navbar to header component and started styling it

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,35 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./style.css";
 
 // Header component. Will return the navbar on each page
 
-export default function Header () {
+export default function Header() {
+  const navRef = useRef();
+
+  // When showNavBar is called, the navbar will be shown or hidden
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
   return (
-    <nav className="navbar">
-      <img src="https://loremflickr.com/150/150" alt="Logo placeholder" />
-      <h1>David Marsh</h1>
-      <ul>
-        <li>Home</li>
-        <li>Projects</li>
-        <li>CV</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-    
+    <>
+      <nav className="navbar" ref={navRef}>
+        <img src="https://loremflickr.com/150/150" alt="Logo placeholder" />
+        <h1>David Marsh</h1>
+
+        <a href="/#">Home</a>
+        <a href="/#">About</a>
+        <a href="/#">Projects</a>
+        <a href="/#">CV</a>
+        <a href="/#">Contact</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavBar}>
+        <FaBars />
+      </button>
+    </>
   );
-};
+}
