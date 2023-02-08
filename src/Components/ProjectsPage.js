1af6b3c70bd6a034f578854eda40fc077f96656d
David Marsh <40401344+Daveym1@users.noreply.github.com>
2023-02-08 12:29:42 +0000

    added cards and styling to projects page

--- src/Components/ProjectsPage.js
+++ src/Components/ProjectsPage.js
@@ -1,102 +1,113 @@
 import React from "react";
+import projects from "../data/projects.json";
 
 export default function ProjectsPage() {
   return (
-    <div class="container text-center">
-      <div class="row">
-        <div class="col-lg-4 col-md-6 col-sm-12">
-          <div class="card" style={{ width: "18rem" }}>
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">
-                Go somewhere
+    <>
+      <div className="row">
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <div className="card" style={{ width: "20rem" }}>
+            <img
+              src={require("../images/teamGenerator.png")}
+              className="card-img-top"
+              alt="Team Generator screenshot"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{projects[0].name}</h5>
+              <p className="card-text">{projects[0].description}}</p>
+              <a href="/#" className="btn btn-primary">
+                More Info
               </a>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-6 col-sm-12">
-          <div class="card" style={{ width: "18rem" }}>
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">
-                Go somewhere
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <div className="card" style={{ width: "20rem" }}>
+            <img
+              src={require("../images/readmeGenerator.png")}
+              className="card-img-top"
+              alt="Team Generator screenshot"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{projects[1].name}</h5>
+              <p className="card-text">{projects[1].description}}</p>
+              <a href="/#" className="btn btn-primary">
+                More Info
               </a>
             </div>
           </div>
         </div>
-        <div class="col">
-          <div class="card" style={{ width: "18rem" }}>
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">
-                Go somewhere
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <div className="card" style={{ width: "20rem" }}>
+            <img
+              src={require("../images/weatherApp.png")}
+              className="card-img-top"
+              alt="Team Generator screenshot"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{projects[2].name}</h5>
+              <p className="card-text">{projects[2].description}}</p>
+              <a href="/#" className="btn btn-primary">
+                More Info
               </a>
             </div>
           </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <div class="card" style={{ width: "18rem" }}>
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">
-                Go somewhere
+        </div>
+
+        <div className="row">
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <div className="card" style={{ width: "20rem" }}>
+            <img
+              src={require("../images/WorkDayApp.png")}
+              className="card-img-top"
+              alt="Team Generator screenshot"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{projects[3].name}</h5>
+              <p className="card-text">{projects[3].description}}</p>
+              <a href="/#" className="btn btn-primary">
+                More Info
               </a>
             </div>
           </div>
         </div>
-        <div class="col">
-          <div class="card" style={{ width: "18rem" }}>
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">
-                Go somewhere
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <div className="card" style={{ width: "20rem" }}>
+            <img
+              src={require("../images/food2go.png")}
+              className="card-img-top"
+              alt="Team Generator screenshot"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{projects[4].name}</h5>
+              <p className="card-text">{projects[4].description}}</p>
+              <a href="/#" className="btn btn-primary">
+                More Info
               </a>
             </div>
           </div>
         </div>
-        <div class="col">
-          <div class="card" style={{ width: "18rem" }}>
-            <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">
-                Go somewhere
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <div className="card" style={{ width: "20rem" }}>
+            <img
+              src={require("../images/teamGenerator.png")}
+              className="card-img-top"
+              alt="Team Generator screenshot"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{projects[5].name}</h5>
+              <p className="card-text">{projects[5].description}}</p>
+              <a href="/#" className="btn btn-primary">
+                More Info
               </a>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+        </div>
+
+        
+    </>
   );
 }
+console.log(projects[0]);
