d3a4523d6a3f658f90cd8c9bdadfcba2ed808b32
David Marsh <40401344+Daveym1@users.noreply.github.com>
2023-02-09 17:58:52 +0000

    updated styling for homepage, added image and text

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function Project({ match }) {
   const project = projects.find((p) => p.id === match.params.id);
-  return <div>Project</div>;
+  return (
+    <div>
+      <h1>{project.name}</h1>
+    </div>
+  );
 }
--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,27 +1,24 @@
 import React from "react";
-import { Link, Route, BrowserRouter as Router } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 function ProjectCard({ project }) {
   return (
-    <Router>
-      <div className="card" style={{ width: "18rem" }}>
-        <img
-          src={project.image}
-          className="card-img-top"
-          alt={`${project.name} screenshot`}
-          style={{ height: "200px", width: "100%" }}
-          key={project.id}
-        />
-        <div className="card-body">
-          <h5 className="card-title">{project.name}</h5>
-          <p className="card-text">{project.description}</p>
-          <Link to={`/project/${project.id}`} className="btn btn-primary">
-            More info
-          </Link>
-        </div>
+    <div className="card" style={{ width: "18rem" }}>
+      <img
+        src={project.image}
+        className="card-img-top"
+        alt={`${project.name} screenshot`}
+        style={{ height: "200px", width: "100%" }}
+        key={project.id}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{project.name}</h5>
+        <p className="card-text">{project.description}</p>
+        <Link to={`/project/${project.id}`} className="btn btn-primary">
+          More info
+        </Link>
       </div>
-    </Router>
+    </div>
   );
 }
 
