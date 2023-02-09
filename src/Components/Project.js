718f9a1260aae7369a844910143560122ec242c1
David Marsh <40401344+Daveym1@users.noreply.github.com>
2023-02-09 05:58:41 +0000

    added routes for individual projects

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,5 +1,6 @@
 import React from "react";
 
-export default function Project() {
+export default function Project({ match }) {
+  const project = projects.find((p) => p.id === match.params.id);
   return <div>Project</div>;
 }
--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,23 +1,27 @@
 import React from "react";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+
 
 function ProjectCard({ project }) {
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img
-        src={project.image}
-        className="card-img-top"
-        alt={`${project.name} screenshot`}
-        style={{ height: "200px", width: "100%" }}
-        key={project.id}
-      />
-      <div className="card-body">
-        <h5 className="card-title">{project.name}</h5>
-        <p className="card-text">{project.description}</p>
-        <a href={`/project/${project.id}`} className="btn btn-primary">
-          More info
-        </a>
+    <Router>
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src={project.image}
+          className="card-img-top"
+          alt={`${project.name} screenshot`}
+          style={{ height: "200px", width: "100%" }}
+          key={project.id}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{project.name}</h5>
+          <p className="card-text">{project.description}</p>
+          <Link to={`/project/${project.id}`} className="btn btn-primary">
+            More info
+          </Link>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
